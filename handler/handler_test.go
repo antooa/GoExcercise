0c9ee087a21ab358e9738adc29fc21e7d0d46f61
antooa <eparis@atlassian.com>
2018-12-22 19:13:50 +0200

    Added downloading from browser

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"log"
+	"os"
 	"testing"
 )
 
@@ -8,16 +10,23 @@ func TestDownloadFile(t *testing.T) {
 
 	 SampleUrl := "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png"
 
-	err := DownloadFile("testfile1", "")
+	err := UploadFile("testfile1", "")
 	expected := `Get : unsupported protocol scheme ""`
 	if err.Error() != expected{
 		t.Error("For", `""`, "expected", expected, "got", err.Error())
 	}
 
-	err = DownloadFile("testfile2", SampleUrl)
+	err = UploadFile("testfile2", SampleUrl)
 	expected = string("<nil>")
 	if err != nil{
 		t.Error("For", `google pic`, "expected", expected, "got", err.Error())
 	}
-
+	err = os.Remove("testfile1")
+	if err != nil{
+		log.Fatal(err)
+	}
+	err = os.Remove("testfile2")
+	if err != nil{
+		log.Fatal(err)
+	}
 }
